c2746bdbc8e8269fb19ee0a08efbdf705b829eae
Veniamin Pyatski <69654071+BenyaPyat@users.noreply.github.com>
2024-05-17 13:48:27 -0400

    update project

---
+++ .venv/Scripts/Reorganize_data_6000.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('new_plates_map.csv')
+x = print(df.size)
+
---
+++ .venv/Scripts/plate_parser.py
@@ -0,0 +1,10 @@
+def empty_val_assignment(k_val, b_val, new_df, x_range, y_range):
+    i = 0
+    for d in new_df:
+        for x in range(x_range):
+           for y in range(y_range):
+               if str(d.iloc[x, y]) == 'nan' and x%2==0:
+                   d.iloc[x, y] = k_val
+                   d.iloc[x+1, y] = b_val
+                   return i, x, y
+        i = i + 1
